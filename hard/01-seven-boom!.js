cf2de9a09fb1c4a26de7e53977ef49d1cac4a04e
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2026-04-30 06:58:45 +0500

    feat: implement sevenBoom function to detect presence of digit 7 in array elements

--- hard/01-seven-boom!.js
+++ hard/01-seven-boom!.js
@@ -14,3 +14,31 @@
 
 // sevenBoom([2, 55, 60, 97, 86]) ➞ "Boom!"
 // // 97 contains the number seven.
+
+const sevenBoom = arr => {
+    let isSevenFound = false;
+
+    for(let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].toString();
+    }
+
+    console.log(arr);
+    console.log(typeof arr[0])
+
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i].includes("7")) {
+            isSevenFound = true;
+            break;
+        }
+    }
+
+    if (!isSevenFound) {
+        return "there is no 7 in the array";
+    } else {
+        return "Boom!"
+    }
+}
+
+console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]));
+console.log(sevenBoom([8, 6, 33, 100]));
+console.log(sevenBoom([2, 55, 60, 97, 86]));
