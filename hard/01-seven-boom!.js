8516cd40ca51b2e2e4063e024343799fd5c99a2d
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2026-04-30 07:02:38 +0500

    refactor: format sevenBoom code and implement countTrue solution

--- hard/01-seven-boom!.js
+++ hard/01-seven-boom!.js
@@ -15,29 +15,29 @@
 // sevenBoom([2, 55, 60, 97, 86]) ➞ "Boom!"
 // // 97 contains the number seven.
 
-const sevenBoom = arr => {
-    let isSevenFound = false;
+const sevenBoom = (arr) => {
+  let isSevenFound = false;
 
-    for(let i = 0; i < arr.length; i++) {
-        arr[i] = arr[i].toString();
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].toString();
+  }
+
+  console.log(arr);
+  console.log(typeof arr[0]);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes("7")) {
+      isSevenFound = true;
+      break;
     }
+  }
 
-    console.log(arr);
-    console.log(typeof arr[0])
-
-    for(let i = 0; i < arr.length; i++) {
-        if (arr[i].includes("7")) {
-            isSevenFound = true;
-            break;
-        }
-    }
-
-    if (!isSevenFound) {
-        return "there is no 7 in the array";
-    } else {
-        return "Boom!"
-    }
-}
+  if (!isSevenFound) {
+    return "there is no 7 in the array";
+  } else {
+    return "Boom!";
+  }
+};
 
 console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]));
 console.log(sevenBoom([8, 6, 33, 100]));
--- medium/01-how-much is-true?.js
+++ medium/01-how-much is-true?.js
@@ -4,13 +4,27 @@
 
 // Create a function which returns the number of true values there are in an array.
 
-Examples
+// Examples
 
 // countTrue([true, false, false, true, false]) ➞ 2
 // countTrue([false, false, false, false]) ➞ 0
 // countTrue([]) ➞ 0
 
-Notes
+// Notes
 
 // Return 0 if given an empty array.
 // All array items are of the type bool (true or false).
+
+const countTrue = (arr) => {
+  let trueCount = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      trueCount++;
+    }
+  }
+  return trueCount;
+};
+
+console.log(countTrue([true, false, false, true, false]));
+console.log(countTrue([false, false, false, false]));
+console.log(countTrue([]));
