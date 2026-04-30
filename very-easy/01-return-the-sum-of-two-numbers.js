6a9c68b4b12cda1b69895b9d4009bf916dc4f242
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2026-04-30 06:40:16 +0500

    feat: add initial Edabit challenge templates across multiple difficulty levels

---
+++ very-easy/01-return-the-sum-of-two-numbers.js
@@ -0,0 +1,23 @@
+// Question 01:
+
+// Return the Sum of Two Numbers
+
+// Create a function that takes two numbers as arguments and returns their sum.
+
+// Examples
+
+// addition(3, 2) ➞ 5
+// addition(-3, -6) ➞ -9
+// addition(7, 3) ➞ 10
+
+// Notes
+
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+const addition = (a, b) => a + b;
+
+console.log(addition(3, 2));
+console.log(addition(-3, -6));
+console.log(addition(7, 3));
