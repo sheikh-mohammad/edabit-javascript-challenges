518556ba94a0d434175a93aa8906264480c07a1f
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2026-05-01 11:44:30 +0500

    feat: add solution for return the next number from the integer passed challenge

---
+++ very-easy/03-return-the-next-number-from-the-integer-passed.js
@@ -0,0 +1,23 @@
+// Question 03:
+
+// Return the Next Number from the Integer Passed
+
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// Examples
+
+// addition(0) ➞ 1
+// addition(9) ➞ 10
+// addition(-3) ➞ -2
+
+// Notes
+
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+const addition = (num) => num + 1;
+
+console.log(addition(0));
+console.log(addition(9));
+console.log(addition(-3));
