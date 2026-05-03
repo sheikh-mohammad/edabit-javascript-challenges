ef6bc98488b25793fa5fad2a714e3ea9f1cf9121
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2026-05-03 17:05:17 +0500

    feat: add solution for finding the minimum and maximum numbers in an array

---
+++ easy/05-find-the-smallest-and-biggest-numbers.js
@@ -0,0 +1,25 @@
+// Question 05:
+
+// Find the Smallest and Biggest Numbers
+
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+// Examples
+
+// minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+// minMax([2334454, 5]) ➞ [5, 2334454]
+// minMax([1]) ➞ [1, 1]
+
+// Notes
+
+// All test arrays will have at least one element and are valid.
+
+const minMax = (arr) => {
+  const smallest = arr.reduce((min, curr) => (min < curr ? min : curr));
+  const largest = arr.reduce((max, curr) => (max > curr ? max : curr));
+  return [smallest, largest];
+};
+
+console.log(minMax([1, 2, 3, 4, 5]));
+console.log(minMax([2334454, 5]));
+console.log(minMax([1]));
