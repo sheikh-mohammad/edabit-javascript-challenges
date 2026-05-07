2d96e69d7cc4df20a59e1ab00d8d230b7c4d6f90
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2026-05-07 20:18:29 +0500

    feat: add power calculator solution for circuit power challenge

---
+++ very-easy/09-power-calculator.js
@@ -0,0 +1,21 @@
+// Question 09:
+
+// Power Calculator
+
+// Create a function that takes voltage and current and returns the calculated power.
+
+// Examples
+
+// circuitPower(230, 10) ➞ 2300
+// circuitPower(110, 3) ➞ 330
+// circuitPower(480, 20) ➞ 9600
+
+// Notes
+
+// Requires basic calculation of electrical circuits (see Resources for info).
+
+const circuitPower = (voltage, current) => voltage * current;
+
+console.log(circuitPower(230, 10));
+console.log(circuitPower(110, 3));
+console.log(circuitPower(480, 20));
