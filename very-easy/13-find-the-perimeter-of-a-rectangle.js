ca2cefc4a336d14a736ad38e884165a138f05f8b
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2026-05-12 19:03:23 +0500

    feat: add solution for find the perimeter of a rectangle challenge

---
+++ very-easy/13-find-the-perimeter-of-a-rectangle.js
@@ -0,0 +1,23 @@
+// Question 13:
+
+// Find the Perimeter of a Rectangle
+
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+
+// Examples
+
+// findPerimeter(6, 7) ➞ 26
+// findPerimeter(20, 10) ➞ 60
+// findPerimeter(2, 9) ➞ 22
+
+// Notes
+
+// Don't forget to return the result.
+// If you're stuck, find help in the Resources tab.
+// If you're really stuck, find solutions in the Solutions tab.
+
+const findPerimeter = (length, width) => length * width;
+
+console.log(findPerimeter(6, 7));
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 9));
