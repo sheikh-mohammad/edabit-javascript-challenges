7f81a519695e2a7d5a18cec10d7769a0534709d6
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2026-05-19 20:02:58 +0500

    fix: resolve infinite loop in printArray by adding increment expression to for-loop

---
+++ very-easy/21-buggy-code-part-5.js
@@ -0,0 +1,40 @@
+// Question 21:
+
+// Buggy Code (Part 5)
+
+// Mubashir created an infinite loop! Help him by fixing the code in the code tab to pass this challenge. Look at the examples below to get an idea of what the function should do.
+
+// Examples
+
+// printArray(1) ➞ [1]
+// printArray(3) ➞ [1, 2, 3]
+// printArray(6) ➞ [1, 2, 3, 4, 5, 6]
+
+// Notes
+
+// READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+// Don't overthink this challenge; it's not supposed to be hard.
+
+// function printArray(number) {
+//   var newArray = [];
+
+//   for(var i = 1; i <= number;) {
+//     newArray.push(i);
+//   }
+
+//   return newArray;
+// }
+
+function printArray(number) {
+  var newArray = [];
+
+  for (var i = 1; i <= number; i++) {
+    newArray.push(i);
+  }
+
+  return newArray;
+}
+
+console.log(printArray(1));
+console.log(printArray(3));
+console.log(printArray(6));
