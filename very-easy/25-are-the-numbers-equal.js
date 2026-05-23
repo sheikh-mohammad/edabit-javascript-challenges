99d716a661d528dc4b7c85580dc095ced8e4b4e9
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2026-05-23 23:10:24 +0500

    feat: add solution for Are the Numbers Equal challenge

---
+++ very-easy/25-are-the-numbers-equal.js
@@ -0,0 +1,21 @@
+// Question 25:
+
+// Are the Numbers Equal?
+
+// Create a function that returns true when num1 is equal to num2; otherwise return false.
+
+// Examples
+
+// isSameNum(4, 8) ➞ false
+// isSameNum(2, 2) ➞  true
+// isSameNum(2, "2") ➞ false
+
+// Notes
+
+// Don't forget to return the result.
+
+const isSameNum = (num1, num2) => (num1 === num2 ? true : false);
+
+console.log(isSameNum(4, 8));
+console.log(isSameNum(2, 2));
+console.log(isSameNum(2, "2"));
